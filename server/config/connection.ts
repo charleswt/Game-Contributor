fe5d3579880e7f4000e27d14fb5fcfa7fe0c9c20
ct <charleswtiffany@gmail.com>
2024-05-20 20:08:23 -0700

    added ts config and pool to connection.ts

---
+++ server/config/connection.ts
@@ -0,0 +1,21 @@
+import { Pool } from 'pg';
+import 'dotenv/config'
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT) || 8000,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database');
+});
+
+pool.on('error', (err: Error) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+export default pool;
