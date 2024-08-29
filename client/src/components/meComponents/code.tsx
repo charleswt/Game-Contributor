38d2c160c5a645ebf5ca2018ff74d8fdf7040407
ct <charleswtiffany@gmail.com>
2024-08-28 22:35:32 -0700

    fixed bug

--- client/src/components/meComponents/code.tsx
+++ client/src/components/meComponents/code.tsx
@@ -1,6 +1,5 @@
 import "../../../public/css/style.css";
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { formatDate } from "../../utils/utils";
 import { useQuery } from "@apollo/client";
 import { GET_PUBLISHED_CODE, GET_PUBLISHED_CODES } from "../../utils/queries";
@@ -18,7 +17,14 @@ interface Code {
 
 export default function MeCode({ paramCompanyId }: { paramCompanyId: any }) {
   const { loading: codeLoading, data: codeData } = useQuery(GET_PUBLISHED_CODE);
-  const [pubdCodes, setPubdCodes] = useState<Code[]>([]);
+  const [pubdCodes, setPubdCodes] = useState<Code[]>([]);  
+  const { loading: loadingCode, data: codesData } = useQuery(
+    GET_PUBLISHED_CODES,
+    {
+      variables: { companyId: paramCompanyId },
+    }
+  );
+  const [allCode, setAllCode] = useState<Code[]>([]);
 
   useEffect(() => {
     if (codeData) {
@@ -26,48 +32,44 @@ export default function MeCode({ paramCompanyId }: { paramCompanyId: any }) {
       console.log(codeData);
     }
   }, [!codeLoading, codeData]);
+  
+  useEffect(() => {
+    if (!loadingCode && codesData) {
+      setAllCode(codesData.publishedCodesByCompany);
+    }
+  }, [loadingCode, codesData]);
+
   if (codeLoading) {
     return <div className="loader"></div>;
   }
-
-  const { loading: loadingCode, data: codesData } = useQuery(
-    GET_PUBLISHED_CODES,
-    {
-      variables: { companyId: paramCompanyId },
-    }
-  );
-  const [allCode, setAllCode] = useState<Code[]>();
-
-  useEffect(() => {
-    if (!loadingCode && codeData) {
-      setAllCode(codesData.publishedCodesByCompany);
-    }
-    console.log(allCode, "93");
-  }, [loadingCode, codeData]);
 
   return (
     <>
       {pubdCodes.length > 0 && <div className="bg">Sent Code</div>}
       {pubdCodes.length > 0 ? (
-        pubdCodes.map((code: Code) => (
-          <div className="bg" key={code.id}>
-            <div>{code.id}</div>
-            <div>{code.userId}</div>
-            <div>{code.companyId}</div>
-            <div>{code.code}</div>
-            <div>{code.createdAt}</div>
-          </div>
-        ))
-      ) : (
-        <div className="bg">You Have No Published Code</div>
-      )}
+            pubdCodes?.map((code: Code) => (
+            <div className="bg" key={code.id}>
+              <p>
+                <strong>User ID:</strong> {code.userId}
+              </p>
+              <p>
+                <strong>Company ID:</strong> {code.companyId}
+              </p>
+              <p>
+                <strong>Code Snippet:</strong> <code>{code.code}</code>
+              </p>
+              <p>
+                <strong>Submitted on:</strong> {formatDate(code.createdAt)}
+              </p>
+            </div>)
+          )):(
+            <div>You Have No Recieved Code</div>
+          )}
 
-      {allCode && <div className="bg">Recieved Code</div>}
-
-      {allCode && (
-        <div className="bg">
-          {allCode?.map((code: Code) => (
-            <div key={code.id}>
+      {allCode.length > 0 && <div className="bg">Recieved Code</div>}
+          {allCode.length > 0 ? (
+            allCode?.map((code: Code) => (
+            <div className="bg" key={code.id}>
               <p>
                 <strong>Submitted by:</strong> {code.firstName} {code.lastName}{" "}
                 (@{code.username})
@@ -84,10 +86,12 @@ export default function MeCode({ paramCompanyId }: { paramCompanyId: any }) {
               <p>
                 <strong>Submitted on:</strong> {formatDate(code.createdAt)}
               </p>
-            </div>
-          ))}
-        </div>
-      )}
+            </div>)
+          )):(
+            <div>You Have No Recieved Code</div>
+          )}
+        
+      
     </>
   );
 }
