d19c589879cbe3e7d9f4ba20bcbbc59f9d5accb0
ct <charleswtiffany@gmail.com>
2024-09-05 13:27:13 -0700

    updated connection so I can host server on aws

--- server/config/connect.ts
+++ server/config/connect.ts
@@ -1,12 +1,21 @@
 import { Pool } from "pg";
 import "dotenv/config";
 
-let pool = new Pool({
+let pool = new Pool(
+  !process.env.PROD?
+  {
     user: process.env.DB_USER,
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT) || 8000,
+    port: Number(process.env.DB_PORT),
+  }:
+  {
+    user: process.env.DB_USER_AWS,
+    host: process.env.DB_HOST_AWS,
+    database: process.env.DB_NAME_AWS,
+    password: process.env.DB_PASSWORD_AWS,
+    port: Number(process.env.DB_PORT_AWS),
   });
 
 pool.on("connect", () => {
