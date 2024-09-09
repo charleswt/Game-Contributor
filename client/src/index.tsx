cef737a73f4b179cbb6d46827b44ad05127ba8a1
ct <charleswtiffany@gmail.com>
2024-09-08 18:48:32 -0700

    removed strict mode on router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -56,7 +56,5 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
 );
